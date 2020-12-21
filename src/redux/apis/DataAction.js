18545e64623b5fce0e64d9836ed92448ebe084e3
hunzalashakeel <hunzala.shakeel@nolinbpo.com>
2020-12-21 17:52:51 +0500

    first push

--- src/redux/apis/DataAction.js
+++ src/redux/apis/DataAction.js
@@ -7,7 +7,7 @@ import {  useSelector } from 'react-redux';
 export const webURL = "http://mergemtvw.herokuapp.com";
 
 // export const url = "http://192.168.4.100:3000";
-export const url = "https://crmserver-development.herokuapp.com";
+export const url = "https://pu-crm-backend-develop.herokuapp.com";
 
 export const uploadUrl = "https://images.vanwala.pk";
 
