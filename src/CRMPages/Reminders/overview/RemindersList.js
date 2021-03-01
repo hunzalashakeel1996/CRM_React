c77e3e001c9f5a557115acfa1ddaa08bd505a340
hunzalashakeel <hunzala.shakeel@nolinbpo.com>
2021-03-01 11:33:23 +0500

    remove all links from sidebar except tickets and reminders

--- src/CRMPages/Reminders/overview/RemindersList.js
+++ src/CRMPages/Reminders/overview/RemindersList.js
@@ -107,7 +107,7 @@ const RemindersList = (props) => {
             {/* <p>{comments.length>0? comments[comments.length-1].shortDesc: 'No comments at this ticket yet'}</p> */}
           </ProjectListTitle>
           ),
-          RefrenceId: <Link to={'#'} onClick={() => {onOpenModal(value)}}><span style={{ color: 'black' }} className="date-started">{RefrenceId !== null ? RefrenceId : '-'}</span></Link>,
+          RefrenceId: <Link to={'#'} onClick={() => {onOpenModal(value)}}><span style={{ color: 'black' }} className="date-started">{RefrenceId !== null && RefrenceId !=='null' ? RefrenceId : '-'}</span></Link>,
           CreateBy: <Link to={'#'} onClick={() => {onOpenModal(value)}}><span style={{ color: 'black' }} className="date-started">{CreateBy}</span></Link>,
           ReminderType: <Link to={'#'} onClick={() => {onOpenModal(value)}}><span style={{ color: 'black' }} className="date-started">{ReminderType}</span></Link>,
           Assigned: <Link to={'#'} onClick={() => {onOpenModal(value)}}><span style={{ color: 'black' }} className="date-started">{Assigned} {TicketGroup && `(${TicketGroup})`}</span></Link>,
