7175448eae1fa389f0ae4cf3e7085a4057fb306d
hunzalashakeel <hunzala.shakeel@nolinbpo.com>
2021-06-07 14:25:16 +0500

    Update SignIn.js

--- src/container/profile/authentication/overview/SignIn.js
+++ src/container/profile/authentication/overview/SignIn.js
@@ -39,18 +39,18 @@ const SignIn = () => {
                   else {
                     data = { ...data[0][0], jwtToken: data[1] }
                     dispatch(getUserRole({ loginid: data.LoginID })).then(dataOne => {
-                      // localStorage.setItem('userRole', JSON.stringify(dataOne))
-                      // localStorage.setItem('user', JSON.stringify(data))
-                      // dispatch(login(data));
-                      // setHeaderWithWebToken()
-                      // setState({ ...state, loader: false })
-                      // dispatch(getVendorName({})).then(departs => {
-                      //   console.log('aaaa', departs)
-                      //   dispatch(addVendorName(departs[0]))
-                      //   // setState({ ...state, departs, loader: false  });
-                      // })
-                      // history.push('/admin');
-                      console.log('aaaa', dataOne)
+                      localStorage.setItem('userRole', JSON.stringify(dataOne))
+                      localStorage.setItem('user', JSON.stringify(data))
+                      dispatch(login(data));
+                      setHeaderWithWebToken()
+                      setState({ ...state, loader: false })
+                      dispatch(getVendorName({})).then(departs => {
+                        console.log('aaaa', departs)
+                        dispatch(addVendorName(departs[0]))
+                        // setState({ ...state, departs, loader: false  });
+                      })
+                      history.push('/admin');
+                      // console.log('aaaa', dataOne)
                     })
                   }
                 })
