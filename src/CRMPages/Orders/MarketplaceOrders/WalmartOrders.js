87840ee275bc5e800ea8c0ed91d7252b20d7b80b
Muhammad-aqib2020 <muhammad.aqib@Nolin.com>
2021-09-27 11:51:13 +0500

    single order work Done

--- src/CRMPages/Orders/MarketplaceOrders/WalmartOrders.js
+++ src/CRMPages/Orders/MarketplaceOrders/WalmartOrders.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
-import { Input, Tabs,Form, Table, Upload, Row, Col, DatePicker, Checkbox, Image, Select,notification,Spin } from 'antd';
+import { Input, Tabs, Form, Table, Upload, Row, Col, DatePicker, Checkbox, Image, Select, notification, Spin } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, BtnGroup } from '../../../components/buttons/buttons';
 import { Drawer } from '../../../components/drawer/drawer';
@@ -9,7 +9,7 @@ import { Main, DatePickerWrapper } from '../../styled';
 import { UploadOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
 import { downloadFile, DownlaodWithReact } from '../../../components/utilities/utilities'
-import { apiWalmartCustomerEmail ,apiWalmartGetOrder} from '../../../redux/apis/DataAction';
+import { apiWalmartCustomerEmail, apiWalmartGetOrder, apiWalmartGetSingleOrder } from '../../../redux/apis/DataAction';
 
 import './MarketplaceOrders.css';
 
@@ -42,13 +42,13 @@ function onSearch(val) {
 const validateMessages = {
     required: '${name} is required!',
     types: {
-      email: '${name} is not validate email!',
-      number: '${name} is not a validate number!',
+        email: '${name} is not validate email!',
+        number: '${name} is not a validate number!',
     },
     number: {
-      range: '${name} must be between ${min} and ${max}',
+        range: '${name} must be between ${min} and ${max}',
     },
-  };
+};
 const MarketplaceOrdersView = (props) => {
     const dispatch = useDispatch();
     const [form] = Form.useForm();
@@ -62,10 +62,11 @@ const MarketplaceOrdersView = (props) => {
         checkData: [],
         checked: null,
         values: {},
-        Pono:[],
+        Pono: [],
         isLoader: false,
+        singlePonumber: []
     });
-    const {Pono,isLoader }= state
+    const { Pono, isLoader, singlePonumber } = state
     const multipleChange = childData => {
         setstate({ ...state, checkData: childData });
     };
@@ -127,13 +128,13 @@ const MarketplaceOrdersView = (props) => {
             key: 'address',
         },
     ];
-    
+
     const getWalmartCustomerEmail = () => {
-        
+
         setstate({ ...state, isLoader: true })
-        dispatch(apiWalmartCustomerEmail({  pono_: Pono })).then(data => {
+        dispatch(apiWalmartCustomerEmail({ pono_: Pono })).then(data => {
             // console.log(data)
-            setstate({ ...state, isLoader: false })         
+            setstate({ ...state, isLoader: false })
             downloadFile(data);
             notification.success({
                 message: 'Successfull Dowload',
@@ -143,163 +144,210 @@ const MarketplaceOrdersView = (props) => {
         })
 
     };
-    const getPOno = (value) =>
-    {
-        console.log('getPOno',value)
+    const getPOno = (value) => {
+        console.log('getPOno', value)
         setstate({ ...state, Pono: value })
     }
-    const getWalmartOrderApi =()=> {
+    const getWalmartSinglePonumber = (value) => {
 
-        setstate({ ...state, isLoader: true});
-        dispatch(apiWalmartGetOrder()).then(data=>{
-                console.log('apiWalmartGetOrder',data)
+        setstate({ ...state, singlePonumber: value })
+    }
+    const getWalmartOrderApi = () => {
+
+        setstate({ ...state, isLoader: true });
+        dispatch(apiWalmartGetOrder()).then(data => {
+            console.log('apiWalmartGetOrder', data)
             notification.success({
                 message: 'Successfull Get Order ',
                 description: `Successfully Walmart Get order`,
                 onClose: close,
             });
-            setstate({ ...state, isLoader: false});
+            setstate({ ...state, isLoader: false });
+        })
+    }
+    const getWalmartSingleOrderApi = () => {
+
+        setstate({ ...state, isLoader: true });
+        dispatch(apiWalmartGetSingleOrder({ POId: singlePonumber })).then(data => {
+            console.log('apiWalmartGetOrder', data)
+            notification.success({
+                message: 'Successfull Get Order ',
+                description: `Successfully Walmart Get order`,
+                onClose: close,
+            });
+            setstate({ ...state, isLoader: false });
         })
     }
     return (
         <>
-         <Spin indicator={<img src="/img/icons/loader.gif" style={{ width: 100, height: 100 }} />} spinning={isLoader} >
-            <Row>
-                <Cards title="Get Walmart Orders (API) - USA" caption="The simplest use of Drawer" >
-                {/* <Form layout="inline" initialValue="" label="" form={form} id="Get Walmart Orders (API) - USA" name="nest-messages"  validateMessages={validateMessages}> */}
-                    <Row gutter={15}>
-                        <Col lg={6} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>StartDate</h3></div>
-                           
-                                <DatePicker style={{ padding: 10, width:'100%', }} onChange={onChange} />
-                           
-                        </Col>
-                        <Col lg={6} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>EndDate</h3></div>
-                         
-                                <DatePicker style={{ padding: 10 }} onChange={onChange} />
-                           
-                        </Col>
+            <Spin indicator={<img src="/img/icons/loader.gif" style={{ width: 100, height: 100 }} />} spinning={isLoader} >
+                <Row >
+                    <Cards title="Get Walmart Orders (API) - USA" caption="The simplest use of Drawer" >
+                        {/* <Form layout="inline" initialValue="" label="" form={form} id="Get Walmart Orders (API) - USA" name="nest-messages"  validateMessages={validateMessages}> */}
+                        <Row style={{ marginBottom: 20 }} gutter={15}>
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>StartDate</h3></div> */}
 
-                        <Col lg={6} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div>
-                        
-                                 <Button size="large"    type="success" >
+                                <DatePicker placeholder="StartDate" style={{ padding: 10, width: '100%', }} onChange={onChange} />
+
+                            </Col>
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>EndDate</h3></div> */}
+
+                                <DatePicker placeholder="EndDate" style={{ padding: 10 }} onChange={onChange} />
+
+                            </Col>
+
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div> */}
+
+                                <Button size="large" type="success" >
                                     GetOrders
-                        </Button> 
-                        </Col>
-                        <Col lg={6} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders Api</h3></div>
-                         
-                                 <Button size="large"   type="success" onClick={getWalmartOrderApi}>
+                        </Button>
+                            </Col>
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders Api</h3></div> */}
+
+                                <Button size="large" type="success" onClick={getWalmartOrderApi}>
                                     GetOrders Api
                         </Button>
 
-                     
-
-                        
-                        </Col>
 
 
-                    </Row>
-                     {/* </Form> */}
 
-                </Cards>
-            </Row>
-            {/* USA WALMART SHEET METHOD */}
-            <Row style={{  }}>
-                <Cards title="Get Walmart Orders (Sheet Method) - USA" caption="The simplest use of Drawer" >
-                    <Row gutter={25}>
-                        <Col lg={8} xs={24}>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>Upload Walmart Order Sheet</h3></div>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}>
-                                {/* <Cards title="Step 2" caption="The simplest use of Drawer"> */}
-                                 <Button size="large"    type="success" htmlType="submit">
-                                    Upload Sheet
+                            </Col>
+
+
+                        </Row>
+                        <hr
+                            style={{
+                                color: 'black',
+                                backgroundColor: 'black',
+                                height: 2
+                            }}
+                        />
+                        <Row style={{ marginTop: 20 }} gutter={15}>
+
+
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>Insert Ponumber</h3></div> */}
+
+                                <TextArea placeholder="Insert Ponumber" style={{ padding: 10 }} onChange={(e) => { getWalmartSinglePonumber(e.target.value) }} />
+
+                            </Col>
+
+                            <Col lg={6} xs={24}  >
+                                {/* <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetSingleOrders Api</h3></div> */}
+
+                                <Button size="large" type="success" onClick={getWalmartSingleOrderApi}>
+                                    GetSingleOrders Api
                         </Button>
-                                <br></br>
-                                <Upload >
-                                     <Button size="large"  style={{ marginTop: 10 }} className="btn-outlined" size="large" type="light" outlined>
-                                        <UploadOutlined /> Click to Upload
+
+
+
+
+                            </Col>
+
+
+                        </Row>
+                        {/* </Form> */}
+
+                    </Cards>
+                </Row>
+                {/* USA WALMART SHEET METHOD */}
+                <Row style={{}}>
+                    <Cards title="Get Walmart Orders (Sheet Method) - USA" caption="The simplest use of Drawer" >
+                        <Row gutter={25}>
+                            <Col lg={8} xs={24}>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>Upload Walmart Order Sheet</h3></div>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}>
+                                    {/* <Cards title="Step 2" caption="The simplest use of Drawer"> */}
+                                    <Button size="large" type="success" htmlType="submit">
+                                        Upload Sheet
+                        </Button>
+                                    <br></br>
+                                    <Upload >
+                                        <Button size="large" style={{ marginTop: 10 }} className="btn-outlined" size="large" type="light" outlined>
+                                            <UploadOutlined /> Click to Upload
                 </Button>
-                                </Upload>
-                                {/* </Cards> */}
-                            </div>
-                        </Col>
+                                    </Upload>
+                                    {/* </Cards> */}
+                                </div>
+                            </Col>
 
-                        <Col lg={8} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}>
-                                 <Button size="large"    type="success" htmlType="Submit">
-                                    GetOrders
+                            <Col lg={8} xs={24}  >
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}>
+                                    <Button size="large" type="success" htmlType="Submit">
+                                        GetOrders
                         </Button>
 
-                            </div>
-                        </Col>
+                                </div>
+                            </Col>
 
 
 
-                    </Row>
+                        </Row>
 
 
-                </Cards>
-            </Row>
-            {/* CANADA WALMART SHEET METHOD */}
-            <Row style={{  }}>
-                <Cards title="Get Walmart Orders (Sheet Method) - Canada" caption="The simplest use of Drawer" >
-                    <Row gutter={25}>
-                        <Col lg={8} xs={24}>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>Upload Walmart Order Sheet</h3></div>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}>
-                                {/* <Cards title="Step 2" caption="The simplest use of Drawer"> */}
-                                 <Button size="large"    type="success" htmlType="submit">
-                                    Upload Sheet
+                    </Cards>
+                </Row>
+                {/* CANADA WALMART SHEET METHOD */}
+                <Row style={{}}>
+                    <Cards title="Get Walmart Orders (Sheet Method) - Canada" caption="The simplest use of Drawer" >
+                        <Row gutter={25}>
+                            <Col lg={8} xs={24}>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>Upload Walmart Order Sheet</h3></div>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}>
+                                    {/* <Cards title="Step 2" caption="The simplest use of Drawer"> */}
+                                    <Button size="large" type="success" htmlType="submit">
+                                        Upload Sheet
                         </Button>
-                                <br></br>
-                                <Upload >
-                                     <Button size="large"  style={{ marginTop: 10 }} className="btn-outlined" size="large" type="light" outlined>
-                                        <UploadOutlined /> Click to Upload
+                                    <br></br>
+                                    <Upload >
+                                        <Button size="large" style={{ marginTop: 10 }} className="btn-outlined" size="large" type="light" outlined>
+                                            <UploadOutlined /> Click to Upload
                 </Button>
-                                </Upload>
-                                {/* </Cards> */}
-                            </div>
-                        </Col>
+                                    </Upload>
+                                    {/* </Cards> */}
+                                </div>
+                            </Col>
 
-                        <Col lg={8} xs={24}  >
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div>
-                            <div className="atbd-drawer" style={{ marginLeft: 0 }}>
-                                 <Button size="large"    type="success" htmlType="Submit">
-                                    GetOrders
+                            <Col lg={8} xs={24}  >
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}><h3>GetOrders</h3></div>
+                                <div className="atbd-drawer" style={{ marginLeft: 0 }}>
+                                    <Button size="large" type="success" htmlType="Submit">
+                                        GetOrders
                         </Button>
 
-                            </div>
-                        </Col>
-                    </Row>
-                </Cards>
-            </Row>
-            {/* WALMART EMAILS */}
-            {/* Check Labels Here Div  */}
-            <Row style={{  }}>
-                <Cards title="Download  Customer Emails" caption="The simplest use of Drawer" >
-                <Form layout="inline" initialValue="" label="" form={form} id="Download Customer Emails" name="nest-messages"  onFinish={getWalmartCustomerEmail}  validateMessages={validateMessages}>
-                    <Row gutter={50}>
-                        <Col lg={15} xs={24}  >
-                        <Form.Item name="Customer Emails" rules={[{ required: true }]}>
-                                <TextArea style={{ padding: 10 }} onChange={(e)=>{getPOno(e.target.value)}}/>
-                                </Form.Item>
-                        </Col>
-                        <Col lg={6} xs={24}  >
-                        <Form.Item>
-                                 <Button size="large"    type="success" htmlType="submit">
-                                    Downlaod
+                                </div>
+                            </Col>
+                        </Row>
+                    </Cards>
+                </Row>
+                {/* WALMART EMAILS */}
+                {/* Check Labels Here Div  */}
+                <Row style={{}}>
+                    <Cards title="Download  Customer Emails" caption="The simplest use of Drawer" >
+                        <Form layout="inline" initialValue="" label="" form={form} id="Download Customer Emails" name="nest-messages" onFinish={getWalmartCustomerEmail} validateMessages={validateMessages}>
+                            <Row gutter={50}>
+                                <Col lg={15} xs={24}  >
+                                    <Form.Item name="Customer Emails" rules={[{ required: true }]}>
+                                        <TextArea style={{ padding: 10 }} onChange={(e) => { getPOno(e.target.value) }} />
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={6} xs={24}  >
+                                    <Form.Item>
+                                        <Button size="large" type="success" htmlType="submit">
+                                            Downlaod
                               </Button>
-                                
-                              </Form.Item>
-                        </Col>
-                    </Row>
-                    </Form>
-                </Cards>
-            </Row>
+
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Cards>
+                </Row>
             </Spin>
 
 
