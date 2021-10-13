49d403de2d27423845ed42d0bf43158bf1a93188
hunzalashakeel <hunzala.shakeel@nolinbpo.com>
2021-10-13 15:50:44 +0500

    Update AddSizeChart.js

--- src/CRMPages/StyleChartTool/overview/AddSizeChart.js
+++ src/CRMPages/StyleChartTool/overview/AddSizeChart.js
@@ -17,30 +17,50 @@ const AddSizeChart = (props) => {
     const [state, setState] = useState({
         loader: true,
         title: '',
-        colorCode: '',
-        numberOfRows: 2,
-        numberOfColumns: 3
+        styleCode: '',
+        numberOfRows: 0,
+        numberOfColumns: 0,
+        sizeChartValues: [],
+        isShowChart: false
     });
-    const { loader, title, colorCode, numberOfRows, numberOfColumns } = state;
+    const { loader, title, styleCode, numberOfRows, numberOfColumns, sizeChartValues, isShowChart } = state;
 
+    const onChangeRowColumn = (value, isRow) => {
+        let tempSizeChartValues = [...sizeChartValues]
+        if(isRow){
+            tempSizeChartValues = [...new Array(parseInt(value))].map(e => new Array(numberOfColumns).fill(''));
+            setState({...state, isShowChart: false, sizeChartValues: [...tempSizeChartValues], numberOfRows: parseInt(value)})
+        }else{
+            tempSizeChartValues = [...new Array(numberOfRows)].map(e => new Array(parseInt(value)).fill(''));
+            setState({...state, isShowChart: false, sizeChartValues: [...tempSizeChartValues], numberOfColumns: parseInt(value)})
+        }
+
+    }
+
+    const onValueChange = (val, row, col) => {
+        console.log(val, row, col)
+        let tempSizeChartValues = [...sizeChartValues]
+        tempSizeChartValues[row][col] = val.target.value
+        console.log('asdas', tempSizeChartValues)
+    }
 
     return (
         <>
             <div style={{ backgroundColor: 'white', padding: 20, borderRadius: 20 }}>
                 <Row gutter={20}>
                     <Col span={12}>
-                        <Input placeholder="Title" onChange={(val) => { console.log('val') }} />
+                        <Input placeholder="Title" onChange={(val) => { setState({...state, title: val.target.value}) }} />
                     </Col>
 
                     <Col span={12}>
-                        <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
+                        <Input placeholder="Style Code" onChange={(val) => { setState({...state, styleCode: val.target.value}) }} />
                     </Col>
                 </Row>
 
                 <Row gutter={20} style={{marginTop: 20}}>
                     <Col span={12}>
-                        <Select  showSearch defaultValue='' style={{ width: '100%' }} onChange={(val) => { setState({...state, numberOfRows: val}) }}>
-                            <Option key=''>Rows</Option>
+                        <Select  showSearch defaultValue='' style={{ width: '100%' }} onChange={(val) => { onChangeRowColumn(val, true) }}>
+                            <Option key=''>Number of Rows</Option>
                             {numbers.map((number) => (
                                 <Option key={number}>{number}</Option>
                             ))}
@@ -48,8 +68,8 @@ const AddSizeChart = (props) => {
                     </Col>
 
                     <Col span={12}>
-                        <Select showSearch defaultValue='' style={{ width: '100%' }} onChange={(val) => { setState({...state, numberOfColumns: val}) }}>
-                            <Option key=''>Columns</Option>
+                        <Select showSearch defaultValue='' style={{ width: '100%' }} onChange={(val) => { onChangeRowColumn(val, false) }}>
+                            <Option key=''>Number of Columns</Option>
                             {numbers.map((number) => (
                                 <Option key={number}>{number}</Option>
                             ))}
@@ -57,63 +77,30 @@ const AddSizeChart = (props) => {
                     </Col>
                 </Row>
 
+                <Row style={{ marginTop: 20 }}>
+                    <Button size="large" type="primary" onClick={() => {setState({...state, isShowChart: true})}}>
+                        Create Chart
+                    </Button>
+                </Row>
 
-                <Row style={{  marginTop: 20 }}>
+
+                {isShowChart && <Row style={{  marginTop: 20 }}>
                     <Col style={{ overflow: 'auto' }}>
                         <div style={{ maxWidth: 1920, borderLeft: 'solid 1px grey' }}>
                             {numbers.map((number, indexRow) => (
                                 numberOfRows >= indexRow + 1 &&
-                                <Row gutter={20} style={{ flexWrap: 'nowrap', width: '100%' }}>
+                                <Row gutter={20} style={{ flexWrap: 'nowrap', width: '100%', margin: 0}}>
                                     {numbers.map((number, indexColumn) => (
                                         (numberOfColumns >= indexColumn + 1 ) &&
-                                        <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey', borderTop: 'solid 1px grey' }} span={4}>
-                                            {<Input disabled={indexRow===0&&indexColumn===0}  onChange={(val) => { console.log('val') }} />}
+                                        <Col style={{ padding: 10, maxWidth: indexColumn===0?150:100, minWidth: indexColumn===0?150:100, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey', borderTop: 'solid 1px grey' }} span={4}>
+                                            {<Input style={{maxHeight: 10, minHeight: 10, fontSize: 12, fontWeight:indexRow===0?'bold':'' }}  onChange={(val) => { onValueChange(val, indexRow, indexColumn) }} />}
                                         </Col>
                                     ))}
                                 </Row>
                             ))}
-
-                            {/* <Row gutter={20} style={{ flexWrap: 'nowrap' }}>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}></Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                            </Row>
-                            <Row gutter={20} style={{ flexWrap: 'nowrap' }}>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col><Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                                <Col style={{ padding: 10, borderRight: 'solid 1px grey', borderBottom: 'solid 1px grey' }} span={4}>
-                                    <Input placeholder="Color Code" onChange={(val) => { console.log('val') }} />
-                                </Col>
-                            </Row> */}
                         </div>
                     </Col>
-                </Row>
+                </Row>}
 
 
             </div>
