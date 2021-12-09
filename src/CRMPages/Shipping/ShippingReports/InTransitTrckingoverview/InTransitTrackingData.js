fc5ab7e3dbcd2d1dbb79cd9bdb9ae3cc2f609161
Muhammad-aqib2020 <muhammad.aqib@Nolin.com>
2021-12-09 12:41:57 +0500

    fixing

--- src/CRMPages/Shipping/ShippingReports/InTransitTrckingoverview/InTransitTrackingData.js
+++ src/CRMPages/Shipping/ShippingReports/InTransitTrckingoverview/InTransitTrackingData.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
-import { Input, Tabs, Form, Table, Upload, Row, Col, DatePicker, Checkbox, Image, notification,Spin } from 'antd';
+import { Input, Tabs, Form, Table, Upload, Row, Col, DatePicker, Checkbox, Image, notification, Spin } from 'antd';
 import { Button, BtnGroup } from '../../../../components/buttons/buttons';
 import { inTransitsTrackingData } from '../../../../redux/apis/DataAction';
 import { Cards } from '../../../../components/cards/frame/cards-frame';
@@ -47,12 +47,12 @@ const InTransitTrackingData = (props) => {
         endDate: '',
         checkAll: false,
         isSellerCheckedList: [],
-        isLoader:false
+        isLoader: false
     });
 
     const sellerName = ['Amazon', 'Walmart', 'sears', 'Ebay', 'NewEgg', 'PU', 'JLC']
     const dispatch = useDispatch()
-    const { states, file, marketplace, startDate, endDate, checkAll, isSellerCheckedList,isLoader } = state
+    const { states, file, marketplace, startDate, endDate, checkAll, isSellerCheckedList, isLoader } = state
     const multipleChange = childData => {
         setState({ ...state, checkData: childData });
     };
@@ -86,7 +86,7 @@ const InTransitTrackingData = (props) => {
                 description: `Successfully Report`,
                 onClose: close,
             });
-          
+
         })
     };
 
@@ -101,14 +101,14 @@ const InTransitTrackingData = (props) => {
             sellerList = []
 
         }
-         console.log(sellerList)
+        console.log(sellerList)
         setState({ ...state, marketplace: sellerList, isSellerCheckedList: e.target.checked ? [...sellerName] : [], checkAll: e.target.checked ? true : false })
 
     };
     let username = [];
 
     const onListCheckChange = (val, i, isChecked) => {
-            console.log(val,i,isChecked)
+        console.log(val, i, isChecked)
 
         if (isChecked) {
             // console.log('if', val)
@@ -132,111 +132,108 @@ const InTransitTrackingData = (props) => {
             temp.splice(index, 1)
 
         }
-    
+
         setState({ ...state, marketplace: temp, isSellerCheckedList: temp, checkAll: sellerName.length === sellerList.length ? true : false });
 
     }
     const onChangeStartDate = (value) => {
-        // console.log(value.format('MM/DD/YYYY'))
+       
         setState({ ...state, startDate: value });
     }
     const onChangeEndDate = (value) => {
-        // console.log(value.format('MM/DD/YYYY'))
+     
         setState({ ...state, endDate: value.format('MM/DD/YYYY') });
     }
     const insertTransitsData = () => {
-         console.log('seller',marketplace.length )
+        console.log('seller', marketplace.length)
 
         if (marketplace.length > 0) {
             setState({ ...state, isLoader: true })
             dispatch(inTransitsTrackingData({ state: states, orderdatefrom: startDate, orderdateto: endDate, marketplace: marketplace })).then(data => {
                 setState({ ...state, isLoader: false })
                 downloadFile(data)
-                //   message.success(`file uploaded Update ${data}`);
+          
                 notification.success({
                     message: `Successfull  ${data}`,
                     description: `Successfully Report`,
                     onClose: close,
                 });
-              //  location.reload();
+          
             })
         }
-        else
-        {
+        else {
             alert('Select Seller')
         }
-        
+
 
     };
     return (
         <>
-         <Spin indicator={<img src="/img/icons/loader.gif" style={{ width: 100, height: 100 }} />} spinning={isLoader} >
-            <Form layout="inline" initialValue="" label="" form={form} id="InTransit" name="nest-messages" onFinish={insertTransitsData} validateMessages={validateMessages}>
+            <Spin indicator={<img src="/img/icons/loader.gif" style={{ width: 100, height: 100 }} />} spinning={isLoader} >
+                <Form layout="inline" initialValue="" label="" form={form} id="InTransit" name="nest-messages" onFinish={insertTransitsData} validateMessages={validateMessages}>
 
-                <Cards title="InTransit Tracking Report" caption="The simplest use of Drawer" >
+                    <Cards title="InTransit Tracking Report" caption="The simplest use of Drawer" >
 
-                    <Row >
-                        <Col span={24} style={{ marginBottom: 15 }}>
-                            <Checkbox checked={checkAll} onChange={onCheckAllChange} >
-                                <strong>Check All</strong>
-                            </Checkbox>
-                        </Col>
-                    </Row>
-                    <Row gutter={50}>
-
-                        {sellerName.map((val, i) => (
-
-                            <Col span={5} style={{ marginLeft: 10, marginTop: 10 }}>
-
-                                {/* <Cards headless > */}
-
-                                <Checkbox checked={isSellerCheckedList.includes(val)} onChange={(e) => { onListCheckChange(val, i, e.target.checked) }}>
-
-                                    {val}
-
-                                    {/* <img src={`/img/icons/${val}.png`} width="70" height="70" style={{marginLeft:10}}/>  */}
-
+                        <Row >
+                            <Col span={24} style={{ marginBottom: 15 }}>
+                                <Checkbox checked={checkAll} onChange={onCheckAllChange} >
+                                    <strong>Check All</strong>
                                 </Checkbox>
-
-                                {/* </Cards> */}
-
                             </Col>
+                        </Row>
+                        <Row gutter={50}>
 
-                        ))}
+                            {sellerName.map((val, i) => (
 
-                    </Row>
-                    <Row gutter={50} style={{ marginTop: 20 }}>
-                        <Col span={8} >
-                            <Form.Item name="StartDate" rules={[{ required: true }]}>
-                                <DatePicker placeholder="StartDate" style={{ padding: 10 }} onChange={(date) => { onChangeStartDate(date) }} />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}  >
-                            <Form.Item name="EndDate" rules={[{ required: true }]}>
-                                <DatePicker placeholder="EndDate" style={{ padding: 10 }} onChange={(date) => { onChangeEndDate(date) }} />
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item name="State input here" rules={[{ required: true }]}>
-                                <TextArea placeholder="State input here" style={{ padding: 10 }} className="custom" value={states} onChange={onChangeTextArea} style={{ height: 50 }} />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Row style={{ marginTop: 20 }}>
-                        <Col span={6}>
-                            <Form.Item>
-                                <Button size='large' type="success" htmlType="Submit"> Report Data</Button>
-                            </Form.Item>
-                        </Col>
-                    </Row>
+                                <Col span={5} style={{ marginLeft: 10, marginTop: 10 }}>
 
 
-                </Cards>
+                                    <Checkbox checked={isSellerCheckedList.includes(val)} onChange={(e) => { onListCheckChange(val, i, e.target.checked) }}>
+
+                                        {val}
+
+                                     
+                                    </Checkbox>
+
+                              
+
+                                </Col>
+
+                            ))}
+
+                        </Row>
+                        <Row gutter={50} style={{ marginTop: 20 }}>
+                            <Col span={8} >
+                                <Form.Item name="StartDate" rules={[{ required: true }]}>
+                                    <DatePicker placeholder="StartDate" style={{ padding: 10 }} onChange={(date) => { onChangeStartDate(date) }} />
+                                </Form.Item>
+                            </Col>
+                            <Col span={8}  >
+                                <Form.Item name="EndDate" rules={[{ required: true }]}>
+                                    <DatePicker placeholder="EndDate" style={{ padding: 10 }} onChange={(date) => { onChangeEndDate(date) }} />
+                                </Form.Item>
+                            </Col>
+                            <Col span={8}>
+                                <Form.Item name="State input here" rules={[{ required: true }]}>
+                                    <TextArea placeholder="State input here" style={{ padding: 10 }} className="custom" value={states} onChange={onChangeTextArea} style={{ height: 50 }} />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{ marginTop: 20 }}>
+                            <Col span={6}>
+                                <Form.Item>
+                                    <Button size='large' type="success" htmlType="Submit"> Report Data</Button>
+                                </Form.Item>
+                            </Col>
+                        </Row>
 
 
-                {/* MARKETPLACE CHECKBOXES  */}
+                    </Cards>
 
-            </Form>
+
+                    {/* MARKETPLACE CHECKBOXES  */}
+
+                </Form>
 
             </Spin>
 
